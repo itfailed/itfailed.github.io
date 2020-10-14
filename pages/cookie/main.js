8926a1f3c4deab24d2352692218a2be80b521d5f
Gooderist <45722503+drewmayo@users.noreply.github.com>
2020-10-14 17:26:36 -0500

    revert changes

--- pages/cookie/main.js
+++ pages/cookie/main.js
@@ -1160,14 +1160,14 @@ Game.Launch=function()
 		Game.Loader.loaded=Game.Init;
 		Game.Loader.Load(['filler.png']);
 	}
-	/*Game.ErrorFrame=function()
+	Game.ErrorFrame=function()
 	{
 		l('javascriptError').innerHTML=
 		'<div class="title">Oops. Wrong address!</div>'+
 		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
 		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
 		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
-	}*/
+	}
 	Game.timedout=false;
 	Game.Timeout=function()
 	{
@@ -14052,16 +14052,19 @@ window.onload=function()
 	
 	if (!Game.ready)
 	{
-		console.log('[=== '+choose([
-			'Oh, hello!',
-			'hey, how\'s it hangin',
-			'About to cheat in some cookies or just checking for bugs?',
-			'Remember : cheated cookies taste awful!',
-			'Hey, Orteil here. Cheated cookies taste awful... or do they?',
-		])+' ===]');
-		Game.Load();
-		//try {Game.Load();}
-		//catch(err) {console.log('ERROR : '+err.message);}
+		if (top!=self) Game.ErrorFrame();
+		else
+		{
+			console.log('[=== '+choose([
+				'Oh, hello!',
+				'hey, how\'s it hangin',
+				'About to cheat in some cookies or just checking for bugs?',
+				'Remember : cheated cookies taste awful!',
+				'Hey, Orteil here. Cheated cookies taste awful... or do they?',
+			])+' ===]');
+			Game.Load();
+			//try {Game.Load();}
+			//catch(err) {console.log('ERROR : '+err.message);}
 		}
 	}
 };
